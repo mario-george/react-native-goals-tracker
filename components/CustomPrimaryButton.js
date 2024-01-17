be6f1dc368d493770904f656f8e6270ca1f84fd1
mario-george <vortexspades@gmail.com>
2024-01-17 10:05:36 +0200

    Add CustomPrimaryButton component

---
+++ components/CustomPrimaryButton.js
@@ -0,0 +1,11 @@
+import { Pressable, StyleSheet } from "react-native";
+import { View, TextInput, Text } from "react-native";
+
+const CustomPrimaryButton = (props) => {
+  return (
+    <View className="rounded-2xl overflow-hidden my-2 mx-2">
+
+    </View>
+  );
+};
+export default CustomPrimaryButton;
