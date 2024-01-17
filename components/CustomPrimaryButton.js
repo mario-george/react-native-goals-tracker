2e18995d2b7e0e70f9f4ceb5bf60666c07ad52c0
mario-george <vortexspades@gmail.com>
2024-01-17 10:05:40 +0200

    Add pressable primary button component

--- components/CustomPrimaryButton.js
+++ components/CustomPrimaryButton.js
@@ -4,7 +4,15 @@ import { View, TextInput, Text } from "react-native";
 const CustomPrimaryButton = (props) => {
   return (
     <View className="rounded-2xl overflow-hidden my-2 mx-2">
-
+      <Pressable
+        className="bg-violet-500 shadow-lg text-lg overflow-hidden  rounded-2xl  py-2 px-12 "
+        onPress={props.pressFunction}
+        android_ripple={{ color: "#ff99009c" }}
+      >
+        <View className="">
+          <Text className="text-white text-lg">{props.children}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
